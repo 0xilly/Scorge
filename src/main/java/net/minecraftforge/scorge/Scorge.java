538145436e79cad8d26637756f1739ef1709ec17
Anthony Anderson <anderson@mdhs.info>
2017-11-08 01:43:24 -0500

    backport to 1.11
    Signed-off-by: Anthony Anderson <anderson@mdhs.info>

--- src/main/java/net/minecraftforge/scorge/Scorge.java
+++ src/main/java/net/minecraftforge/scorge/Scorge.java
@@ -2,5 +2,5 @@ package net.minecraftforge.scorge;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(name = "Scorge", modid = "scorge", version = "1.0.0", acceptedMinecraftVersions = "1.11.2", dependencies = "required-after:forge@[13.20.1.2530,)")
+@Mod(name = "Scorge", modid = "scorge", version = "1.0.0", acceptedMinecraftVersions = "1.11", dependencies = "required-after:forge@[13.19.1.2199,)")
 public class Scorge{}
